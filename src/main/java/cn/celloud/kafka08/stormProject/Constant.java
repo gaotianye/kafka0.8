b681674f3ec47326fe5f464c51d7318d64b4c913
gaotianye <15156506755@163.com>
2017-06-06 12:21:00 +0800

    update:添加新的常量

--- src/main/java/cn/celloud/kafka08/stormProject/Constant.java
+++ src/main/java/cn/celloud/kafka08/stormProject/Constant.java
@@ -3,4 +3,8 @@ package cn.celloud.kafka08.stormProject;
 public class Constant {
 	//topic
 	static final String TOPIC = "storm_topic_20170606";
+	//kafka_connect
+	static final String KAFKA_CONNECT = "master:9092,slave1:9092,slave2:9092";
+	//serializer_class
+	static final String SERIALIZER_CLASS = "kafka.serializer.StringEncoder";
 }
