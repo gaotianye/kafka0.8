19d35f913a4908b977eb6f869b07341f1c6f226c
gaotianye <15156506755@163.com>
2017-06-12 17:54:56 +0800

    update:常量

--- src/main/java/cn/celloud/kafka08/stormProject/Constant.java
+++ src/main/java/cn/celloud/kafka08/stormProject/Constant.java
@@ -1,8 +1,13 @@
 package cn.celloud.kafka08.stormProject;
 
+import java.util.Date;
+
+import cn.celloud.kafka08.stormProject.utils.DateUtils;
+
+
 public class Constant {
 	//topic
-	static final String TOPIC = "storm_topic_20170606";
+	static final String ORDER_TOPIC = "storm_order_topic_"+DateUtils.formatDate2(new Date());
 	//kafka_connect
 	static final String KAFKA_CONNECT = "master:9092,slave1:9092,slave2:9092";
 	//serializer_class
