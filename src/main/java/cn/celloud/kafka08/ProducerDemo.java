0b81ed26c72016e68df7dc92c80940e14c7f2ea5
gaotianye <15156506755@163.com>
2017-06-12 17:58:12 +0800

    update：修改测试代码

--- src/main/java/cn/celloud/kafka08/ProducerDemo.java
+++ src/main/java/cn/celloud/kafka08/ProducerDemo.java
@@ -22,7 +22,7 @@ public class ProducerDemo {
 		ArrayList<KeyedMessage<String, String>> list = new ArrayList<KeyedMessage<String, String>>();
 		String topic = "gaotianye_test3";
 		//3.下面的key要和生产者的key一致
-		for(int i = 1;i<=1000;i++){
+		/*for(int i = 1;i<=1000;i++){
 			int j = 0;
 			if(i%3==0){
 				j = 3;
@@ -33,12 +33,19 @@ public class ProducerDemo {
 			}
 			KeyedMessage<String, String> message = new KeyedMessage<String, String>(topic,"key"+j,"ggg"+i);
 			list.add(message);
+		}*/
+		String m = "xxx";
+		int i = 0;
+		while(true){
+			i++;
+			KeyedMessage<String, String> message = new KeyedMessage<String, String>(topic,m+i);
+			producer.send(message);
 		}
 		//4.发送一个
 //		producer.send(message1);
 		//5.发送多个
-		producer.send(list);
+//		producer.send(list);
 		//6.关闭
-		producer.close();
+//		producer.close();
 	}
 }
